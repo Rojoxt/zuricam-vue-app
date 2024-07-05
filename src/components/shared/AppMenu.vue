93b2984282c37542a35d8810f335d0444ddf8d76
Rojoxt <delamarzp@gmail.com>
2024-07-05 18:36:35 -0500

    feat: add shared components

---
+++ src/components/shared/AppMenu.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref} from "vue";
+const visible = { type:Boolean,required:true }
+const items= ref([
+  {label:'Home', items:[{
+    label:'Dashboard', icon:'pi pi-fw pi-home', to:'/'}]},
+  {
+    label: 'UI Components',
+    items: [
+      { label: 'Gestión de cámaras', icon: 'pi pi-fw pi-camera', to: '/uikit/formlayout' },
+      { label: 'Gestión de usuarios', icon: 'pi pi-fw pi-users', to: '/uikit/input' },
+      { label: 'Gestión de unidades', icon: 'pi pi-fw pi-car', to: '/uikit/floatlabel' },
+      { label: 'Gestión de incidencias', icon: 'pi pi-fw pi-file', to: '/uikit/invalidstate' },
+      { label: 'Gestión', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+      { label: 'Gestión', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
+
+
+    ]
+  },
+
+])
+</script>
+
+<template>
+  <div class="p-5 h-full">
+    <Menu v-model:visible="visible" :model="items" class="h-full w-80 overflow-y-auto"/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/AppTopbar.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <Menubar class="w-full">
+      <template #start>
+        <img src="../../assets/images/logo.png" alt="Logo" class="h-8">
+        <Button icon="pi pi-bars" aria-label="Submit" @click="$emit('toggle-sidebar')"/>
+      </template>
+
+      <template #end>
+        <div class="flex items-center gap-2">
+          <InputText placeholder="Search" type="text" class="w-32 sm:w-auto"/>
+          <Avatar image="/images/avatar/amyelsner.png" shape="circle"/>
+        </div>
+      </template>
+    </Menubar>
+</template>
+
+<script setup>
+
+
+</script>
+
+
+<style scoped>
+
+</style>
